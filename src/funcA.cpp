0075349d3d8d4913807f2393997d2d594c71af61
Dan Cao <dancao@Dans-MBP.attlocal.net>
2021-03-06 17:57:23 -0800

    Add template in the comment

--- src/funcA.cpp
+++ src/funcA.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
-int func_a(int a, int b = 1)
-{ 
+
+int func_a(int a, int b = 1) {
   int c = a + b;
   std::cout << c <<  std::endl;
   return c;
 }
+
+/*
+template <class T>
+T func_a(T a, T b = 1)
+{
+  T c = a + b;
+  std::cout << c <<  std::endl;
+  return c;
+}
+*/
